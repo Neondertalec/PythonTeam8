1845548e54273e5c51d924592ba0c6b690c7cf15
Dep <viktors.idk@gmail.com>
2026-04-21 02:23:41 +0300

    clean

--- pd5/5_3.py
+++ pd5/5_3.py
@@ -8,15 +8,6 @@ while lon == None:
 	try: lon = float(input("lon: "))
 	except:pass
 
-
-resp = requests.get(f"https://nominatim.openstreetmap.org/reverse", params={
-	'lat': lat,
-	'lon': lon,
-	'format': "json"
-}, headers={
-	"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/147.0.0.0 Safari/537.36"
-})
-
 def deepFind(json_data, *paths, default = "", validate = lambda data: bool(data.strip())):
 	for path in paths:
 		if isinstance(path, str):
@@ -34,9 +25,17 @@ def deepFind(json_data, *paths, default = "", validate = lambda data: bool(data.
 	
 	return default
 
+
+resp = requests.get(f"https://nominatim.openstreetmap.org/reverse", params={
+	'lat': lat,
+	'lon': lon,
+	'format': "json"
+}, headers={
+	"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/147.0.0.0 Safari/537.36"
+})
 resp.raise_for_status()
 json_data = resp.json()
-print(json_data)
+
 print('Name of the place: ' + deepFind(json_data, 'name', ['address', 'isolated_dwelling'], ['address', 'suburb'], ['address', 'village'], default='unset'))
 print('Type: ' + deepFind(json_data, 'type', 'class', default='unset', validate=lambda data: bool(data.strip()) and data != 'yes' ))
 print('House number: ' + deepFind(json_data, ['address', 'house_number'], default='unset'))
